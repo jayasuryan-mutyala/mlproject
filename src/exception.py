91c7970cb08986f2482c349379eae4cc15403f60
jayasuryan-mutyala <jsm.dgme@gmail.com>
2025-10-02 19:45:20 +0800

    minor updates required for logging

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys 
+
+def error_message_details(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    # exc.info provides which info in which line and file we are getting errors 
+    file_name = exc_tb.tb_frame.f_code.co_filename # we get the file name
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+    )
+    
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
